e64ccc58f09f6aa538fdfe18ad19e1754370655f
ArtemKruykov <artem_kruykov@mail.ru>
2025-04-17 14:50:33 +0300

    №3

---
+++ task3.1/Point.cpp
@@ -0,0 +1,37 @@
+﻿#include "point.h"
+
+Point::Point(unsigned int x, unsigned int y) : x(x), y(y) {}
+
+unsigned int Point::getX() const { return x; }
+unsigned int Point::getY() const { return y; }
+
+bool Point::operator==(const Point& other) const {
+    return x == other.x && y == other.y;
+}
+
+bool Point::operator!=(const Point& other) const {
+    return !(*this == other);
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& point) {
+    os << "(" << point.x << ", " << point.y << ")";
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Point& point) {
+    char ch;
+    is >> ch; // читаем '('
+    is >> point.x;
+    is >> ch; // читаем ','
+    is >> point.y;
+    is >> ch; // читаем ')'
+    return is;
+}
+
+Point Point::operator+(unsigned int value) const {
+    return Point(x + value, y + value);
+}
+
+Point Point::operator-(unsigned int value) const {
+    return Point(x - value, y - value);
+}
---
+++ task3.1/main.cpp
@@ -0,0 +1,31 @@
+﻿#include "polygon.h"
+#include <locale>
+#include <iostream>
+#include <vector>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    try {
+        const unsigned int MAX_COORD = 1920; // Пример максимального разрешения
+
+        // Создание многоугольника с помощью точек
+        std::vector<Point> points = { Point(100, 100), Point(200, 100), Point(200, 200), Point(100, 200) };
+        Polygon polygon1(points, MAX_COORD);
+        polygon1.draw();
+
+        // Создание многоугольника с помощью пар чисел
+        std::vector<std::pair<unsigned int, unsigned int>> coords = { {300, 300}, {400, 300}, {400, 400}, {300, 400} };
+        Polygon polygon2(coords, MAX_COORD);
+        polygon2.draw();
+
+        // Чтение многоугольника из ввода
+        Polygon polygon3 = Polygon::ReadFromInput(MAX_COORD);
+        polygon3.draw();
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
