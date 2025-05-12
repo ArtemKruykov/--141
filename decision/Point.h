b09ef489dbfb4a7617b516e29cec9a6121d0c934
ArtemKruykov <artem_kruykov@mail.ru>
2025-05-12 15:17:07 +0300

    Док

--- decision/Point.h
+++ decision/Point.h
@@ -7,15 +7,52 @@ private:
     double y;
 
 public:
+    /**
+     * @brief Конструктор точки.
+     * @param x Координата X.
+     * @param y Координата Y.
+     */
     Point(double x = 0, double y = 0);
 
+    /**
+     * @brief Возвращает координату X.
+     * @return Значение координаты X.
+     */
     double getX() const;
+
+    /**
+     * @brief Возвращает координату Y.
+     * @return Значение координаты Y.
+     */
     double getY() const;
 
+    /**
+     * @brief Оператор сравнения точек.
+     * @param other Другая точка для сравнения.
+     * @return True если точки равны.
+     */
     bool operator==(const Point& other) const;
+    
+    /**
+     * @brief Оператор неравенства точек.
+     * @param other Другая точка для сравнения.
+     * @return True если точки не равны.
+     */
     bool operator!=(const Point& other) const;
 
+    /**
+    * @brief Оператор вывода точки в поток.
+    * @param os Выходной поток.
+    * @param point Точка для вывода.
+    * @return Ссылка на поток.
+    */
     friend std::ostream& operator<<(std::ostream& os, const Point& point);
-    friend std::istream& operator>>(std::istream& is, Point& point);
 
+    /**
+    * @brief Оператор ввода точки из потока.
+    * @param is Входной поток.
+    * @param point Точка для ввода.
+    * @return Ссылка на поток.
+    */
+    friend std::istream& operator>>(std::istream& is, Point& point);
 };
--- decision/Shape.h
+++ decision/Shape.h
@@ -3,8 +3,24 @@
 
 class Shape {
 public:
+    /**
+    * @brief Виртуальный деструктор Shape.
+    */
     virtual ~Shape() = default;
+
+    /**
+    * @brief Возвращает строковое представление фигуры.
+    * @return Строка с описанием фигуры.
+    */
     virtual std::string toString() const = 0;
+
+    /**
+     * @brief Выводит описание фигуры в стандартный вывод.
+     */
     void draw() const;
+
+    /**
+     * @brief Читает параметры фигуры из входного потока.
+     */
     virtual void readFromInput() = 0;
 };
